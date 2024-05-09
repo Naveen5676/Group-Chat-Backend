2089b3bf253c9220ddac606c3773dbc0d99f419f
Naveen5676 <sample.first@yahoo.com>
2024-05-10 00:30:15 +0530

    storing messages in local storage

--- Controllers/Chat.js
+++ Controllers/Chat.js
@@ -1,27 +1,33 @@
-const chatModal = require('../Models/Chat')
+const chatModal = require("../Models/Chat");
+const {Op} = require('sequelize')
 
-exports.AddData = async (req,res,next)=>{
-    const userid = req.user.id
-    const messagebody = req.body.message
+exports.AddData = async (req, res, next) => {
+  const userid = req.user.id;
+  const messagebody = req.body.message;
 
-    chatModal.create({
-        message:messagebody,
-        userAuthDatumId:userid
-    }).then(()=>{
-        res.status(200).json({message: 'message added to db'})
-    }).catch((err)=>{
-        res.status(400).json({message:'error during adding to db', error:err})
+  chatModal
+    .create({
+      message: messagebody,
+      userAuthDatumId: userid,
     })
-    
-}
+    .then(() => {
+      res.status(200).json({ message: "message added to db" });
+    })
+    .catch((err) => {
+      res
+        .status(400)
+        .json({ message: "error during adding to db", error: err });
+    });
+};
 
-exports.sendChatData = async(req,res,next)=>{
-try{    const messageData =await  chatModal.findAll()
+exports.sendChatData = async (req, res, next) => {
+  try {
+    const latestmessageId = req.query.latestmessageId;
+    console.log('latestmessage id ', latestmessageId)
+    const messageData = await chatModal.findAll({where : { id :{ [Op.gt]: latestmessageId}}});
     //console.log('message data ============>>>>>>>>>>',messageData)
-    if(messageData){
-        res.status(200).json(messageData)
-    }}
-    catch(error){
-
+    if (messageData) {
+      res.status(200).json(messageData);
     }
-}
+  } catch (error) {}
+};
