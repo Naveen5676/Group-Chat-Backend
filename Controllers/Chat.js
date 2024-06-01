e09b9160a7364cee0d8563d09fa10e4bd856e066
Naveen5676 <sample.first@yahoo.com>
2024-06-01 23:27:45 +0530

    changed web sockets

--- Controllers/Chat.js
+++ Controllers/Chat.js
@@ -1,80 +1,58 @@
 const chatModal = require("../Models/Chat");
-const {Op} = require('sequelize')
-const {instrument} = require('@socket.io/admin-ui')
-// const multer = require('multer');
+const { Op } = require('sequelize');
+const { instrument } = require('@socket.io/admin-ui');
 const formidable = require("formidable");
 const path = require('path');
 const fs = require('fs');
-const S3services = require('../services/S3services')
+const S3services = require('../services/S3services');
 
 const io = require("socket.io")(5000, {
-  cors:{
+  cors: {
     origin: ["http://localhost:5173", "https://admin.socket.io"],
   }
-})
+});
 
-io.on("connection" , (socket)=>{
-  socket.on("getChats", async(latestmessageId , groupid )=>{
-    try{
-      if(groupid>0){
-        const groupmessagedata = await  chatModal.findAll({where : { id :{[Op.gt]: latestmessageId} , groupDatumGroupid : groupid}})
-        io.emit('groupmessagedata', groupmessagedata)
-     }else{
-      const messageData = await chatModal.findAll({where : { id :{ [Op.gt]: latestmessageId} , groupDatumGroupid: null}});
-      //console.log('message data ============>>>>>>>>>>',messageData)
-      if (messageData) {
-        io.emit('groupmessagedata', messageData)
+io.on("connection", (socket) => {
+  //console.log("A user connected");
+
+  // When a user wants to join public chat
+  socket.on("joinPublic", () => {
+    socket.join("public");
+    //console.log("User joined public chat");
+  });
+
+  // When a user selects a group, they join that group's room
+  socket.on("joinGroup", (groupid) => {
+    socket.join(`group_${groupid}`);
+    //console.log(`User joined group: ${groupid}`);
+  });
+
+  // Listen for getChats event to fetch messages
+  socket.on("getChats", async (latestmessageId, groupid) => {
+    try {
+      let chatData = [];
+      if (groupid > 0) {
+        // Group-specific chat
+        chatData = await chatModal.findAll({
+          where: { id: { [Op.gt]: latestmessageId }, groupDatumGroupid: groupid }
+        });
+        //console.log('group message chat', chatData);
+        socket.emit('groupmessagedata', chatData); // Emit only to the specific group
+      } else {
+        // Public chat
+        chatData = await chatModal.findAll({
+          where: { id: { [Op.gt]: latestmessageId }, groupDatumGroupid: null }
+        });
+        //console.log('public message chat', chatData);
+        socket.emit('publicmessagedata', chatData); // Emit only to the public chat
       }
+    } catch (error) {
+      console.log(error);
     }
+  });
+});
 
-    }catch(error){
-      console.log(error)
-    }
-  })
-})
-
-instrument(io , {auth:false})
-
-// // Set up storage configuration for multer
-// const storage = multer.diskStorage({
-//   destination:(req, file , cb)=>{
-//     cb(null , 'uploads/')
-//   },
-//   filename:(req, file , cb)=>{
-//     cb(null , file.originalname);
-//   }
-// })
-
-// // Initialize multer with the storage configuration
-// const upload = multer({ storage })
-
-
-// exports.AddData =async (req, res, next) => {
-
-
-//   const userid = req.user.id;
-//   const messagebody = req.body.message;
-//   const groupid = req.body.groupid;
-
-
-//   chatModal
-//     .create({
-//       message: messagebody,
-//       userAuthDatumId: userid,
-//       groupDatumGroupid:groupid
-//     })
-//     .then(() => {
-//       res.status(200).json({ message: "message added to db" });
-//     })
-//     .catch((err) => {
-//       res
-//         .status(400)
-//         .json({ message: "error during adding to db", error: err });
-//     });
-// };
-
-
-
+// Handle new message addition
 exports.AddData = async (req, res, next) => {
   const form = new formidable.IncomingForm();
 
@@ -83,72 +61,68 @@ exports.AddData = async (req, res, next) => {
       return res.status(400).json({ message: "Error parsing the files", error: err });
     }
 
-    // console.log('Fields:', fields);
-    // console.log('Uploaded file:', files.file);
-    // console.log('file name ===>', files.file[0].originalFilename )
-    // console.log('file path ===>', files.file[0].filepath )
-
     const messagebody = fields.message;
-    const groupid = fields.groupid ? Number(fields.groupid) : null;  // Set to null if not provided
+    const groupid = fields.groupid ? Number(fields.groupid) : null;
     const userid = req.user.id;
 
-    // console.log('message body ', String(messagebody));
-    // console.log('user id ', userid);
-    // console.log('groupid', groupid);
-
     const chatData = {
-      message:  String(messagebody),
+      message: String(messagebody),
       userAuthDatumId: userid
-    }
+    };
 
-    if(groupid){
+    if (groupid) {
       chatData.groupDatumGroupid = groupid;
     }
 
-    if(files.file){
-      //read they file content
-      const fileContent = fs.readFileSync(String(files.file[0].filepath))
-      const filename =   files.file[0].originalFilename
-      const fileURl = await S3services.uploadtoS3(fileContent , filename)
-      console.log('file url after uploaded to aws', fileURl)
+    if (files.file) {
+      const fileContent = fs.readFileSync(String(files.file[0].filepath));
+      const filename = files.file[0].originalFilename;
+      const fileURl = await S3services.uploadtoS3(fileContent, filename);
       chatData.fileurl = fileURl;
-      chatData.filename = filename
+      chatData.filename = filename;
     }
 
-    chatModal.create(chatData)
-    .then(() => {
+    try {
+      const newMessage = await chatModal.create(chatData);
       res.status(200).json({ message: "Message and file uploaded successfully", file: files.file });
-    })
-    .catch((err) => {
+
+      // Emit the new message to all clients in the relevant room
+      if (groupid) {
+        const chatData = await chatModal.findAll({
+          where: { groupDatumGroupid: groupid }
+        });
+        io.to(`group_${groupid}`).emit('groupmessagedata', chatData);
+      } else {
+        const chatData = await chatModal.findAll({
+          where: { groupDatumGroupid: null }
+        });
+        io.to("public").emit('publicmessagedata', chatData);
+      }
+    } catch (err) {
       res.status(400).json({ message: "Error during adding to db", error: err });
-      console.log(err)
-    });
+      console.log(err);
+    }
   });
 };
-
 
 exports.sendChatData = async (req, res, next) => {
   try {
     const latestmessageId = req.query.latestmessageId;
-    console.log('latestmessage id ', latestmessageId)
-    const groupid = req.query.groupid
-    console.log('group id ', groupid)
-    if(groupid>0){
-       const groupmessagedata = await  chatModal.findAll({where : { id :{[Op.gt]: latestmessageId} , groupDatumGroupid : groupid}})
-       res.status(200).json(groupmessagedata)
-    }else{
-      const messageData = await chatModal.findAll({where : { id :{ [Op.gt]: latestmessageId} , groupDatumGroupid: null}});
-      //console.log('message data ============>>>>>>>>>>',messageData)
+    const groupid = req.query.groupid;
+    if (groupid > 0) {
+      const groupmessagedata = await chatModal.findAll({
+        where: { id: { [Op.gt]: latestmessageId }, groupDatumGroupid: groupid }
+      });
+      res.status(200).json(groupmessagedata);
+    } else {
+      const messageData = await chatModal.findAll({
+        where: { id: { [Op.gt]: latestmessageId }, groupDatumGroupid: null }
+      });
       if (messageData) {
         res.status(200).json(messageData);
       }
     }
-    // const messageData = await chatModal.findAll({where : { id :{ [Op.gt]: latestmessageId}}});
-    // //console.log('message data ============>>>>>>>>>>',messageData)
-    // if (messageData) {
-    //   res.status(200).json(messageData);
-    // }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
